66d0268b2d5b30eb65b42e10095ae1a73bbae520
appworld-hu <burai.sandor@gmail.com>
2022-04-09 18:03:07 +0200

    sanyi dev 2

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -19,7 +19,7 @@ class ProductController extends Controller
                 'seo_title'=>'nullable|min:2|max:80', 
                 'seo_keywords'=>'nullable|min:2|max:200', 
                 'seo_description'=>'nullable|min:2|max:200',
-                'price'=>'required', 
+                'price'=>'required',    
                 'hotprice'=>'nullable', 
         ]);
 
